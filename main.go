fb5f0f17162ed2aee42f49d8aa69a32c6b4f2152
johnnyjong823 <johnnyjong823@gmail.com>
2017-02-10 10:58:44 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,3 @@
-﻿// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -22,7 +10,7 @@ import (
 )
 
 var bot *linebot.Client
-var PetDB string
+
 func main() {
 	var err error
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
@@ -49,9 +37,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				//PetDB = Send() 
-				//var text = PetDB;
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.id +":")).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
