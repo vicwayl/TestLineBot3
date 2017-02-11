aee975c7ba17e547c62a64113463750e92648549
johnnyjong823 <johnnyjong823@gmail.com>
2017-02-11 21:49:17 +0800

    Update main.go

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 				var txt = Send();
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!"+txt+"  "+event.ReplyToken)).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!"+txt+"  "+event.Source.UserId)).Do(); err != nil {
 					log.Print(err)
 				}
 			}
