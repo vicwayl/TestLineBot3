15040f6d6e9497d0f27aee8448889af1bc9d2551
johnnyjong823 <johnnyjong823@gmail.com>
2017-03-06 10:34:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -15,6 +15,9 @@ func main() {
 	var err error
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	log.Println("Bot:", bot, " err:", err)
+	if _, err := bot.PushMessage("U2c68fd429a99dceccc8956571baa7d00", linebot.NewTextMessage("hello")).Do(); err != nil {
+		log.Print(err)
+	}
 	http.HandleFunc("/callback", callbackHandler)
 	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
@@ -32,9 +35,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	if _, err := bot.PushMessage("db2293507c874413ade1f0d52b3fdbec", linebot.NewTextMessage("hello")).Do(); err != nil {
-		log.Print(err)
-	}
+	
 	
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
