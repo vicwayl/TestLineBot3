0d97ed94c8d35863f46fc41a66332439410c56f4
johnnyjong823 <johnnyjong823@gmail.com>
2017-03-06 10:30:14 +0800

    Update main.go

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	if _, err := bot.PushMessage("U2c68fd429a99dceccc8956571baa7d00", linebot.NewTextMessage("hello")).Do(); err != nil {
+	if _, err := bot.PushMessage("db2293507c874413ade1f0d52b3fdbec", linebot.NewTextMessage("hello")).Do(); err != nil {
 		log.Print(err)
 	}
 	
