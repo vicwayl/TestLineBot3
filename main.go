2c1a862bdf7dcc8ab81e83395a11bcc729603e3b
vicwayl <asd108913@gmail.com>
2017-03-27 22:27:56 +0800

    Update main.go

--- main.go
+++ main.go
@@ -34,14 +34,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	if content != nil && content.IsOperation && content.OpType == OpTypeAddedAsFriend {
-		
-			out := fmt.Sprintf("(Welcom MSG)歡迎訊息..")
-			//result.RawContent.Params[0] is who send your bot friend added operation, otherwise you cannot get in content or operation content.
-			_, err = bot.SendText([]string{result.RawContent.Params[0]}, out)
-			if err != nil {
-				log.Println(err)
-			}
+	
 	//if _, err := bot.PushMessage("U2c68fd429a99dceccc8956571baa7d00", linebot.NewTextMessage("hello")).Do(); err != nil {
 	//	txt= txt + err.Error()
 	//}
@@ -51,11 +44,11 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			case *linebot.TextMessage:
 				//var txt = Send(message.Text);
 				//rand.Seed(99)
-				answers := []string{"彥達好帥","彥達好棒","彥達好有錢","彥達開跑車","彥達住豪宅","彥達100分","彥達高材生","彥達金城武","彥達劉德華"}
+				answers := []string{"您好!我是鐵蛋機器人","歡迎來找我玩喔~","工程師哥哥真帥","我最喜歡導覽姐姐了<3","我是阿呆博士的機器人!!"}
 				//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!"+txt+"  "+event.Source.UserID+"   "+event.ReplyToken)).Do(); err != nil {
 				//	log.Print(err)
 				//}
-				var txt = message.Text+","+answers[rand.Intn(len(answers))]
+				var txt =answers[rand.Intn(len(answers))]
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(txt)).Do(); err != nil {
 					log.Print(err)
 				}
